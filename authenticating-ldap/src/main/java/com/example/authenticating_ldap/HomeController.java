48b40f8e6b457ad06c6480c4f155c496ff19715f
jessika <jessikafujimura@gmail.com>
2024-07-11 23:24:56 -0300

    feat: add modules authenticating-ldap

---
+++ authenticating-ldap/src/main/java/com/example/authenticating_ldap/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.authenticating_ldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")
+    public String index(){
+        return "Welcome to my home page\n" +
+                "You logon successful!";
+    }
+}
---
+++ authenticating-ldap/src/main/java/com/example/authenticating_ldap/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.authenticating_ldap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests((auth) -> auth.anyRequest().fullyAuthenticated()).formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+    
+    @Autowired
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+         authenticationManagerBuilder.ldapAuthentication()
+                 .userDnPatterns("uid={0},ou=people")
+                 .groupSearchBase("ou=groups")
+                 .contextSource()
+                 .url("ldap://localhost:8389/dc=springframework,dc=org")
+                 .and()
+                 .passwordCompare()
+                 .passwordEncoder(new BCryptPasswordEncoder())
+                 .passwordAttribute("userPassword");
+    }
+}
